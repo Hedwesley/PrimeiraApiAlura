8d37ac7fbbc0ce6aed299a38f37d76ece284cc19
Hedwesley <hedwesleygusmao@gmail.com>
2025-06-29 23:51:26 -0300

    Atualização

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express"
+
+const app = express ();
+
+const livros = [
+    {
+        id: 1,
+        titulo: "O Senhor dos aneís"
+    },
+    {
+        id: 2,
+        titulo: "O Hobitt"
+    }
+]
+
+app.get("/", (req, res) =>{
+    res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+export default app;
