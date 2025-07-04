2e1769f2ec08138e64ea40b17b3ac43f8b0be22e
Hedwesley <hedwesleygusmao@gmail.com>
2025-07-04 16:53:43 -0300

    Metodo Put e rotas aplicadas

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -9,7 +9,17 @@ class livroController {
             res.status(500).json({ message: `${erro.message} - falha na requisição`});
         }
     };
-
+    
+    static async listarLivroPorId(req, res) {
+        try{
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na requisição do livro`});
+        }
+    };
+    
     static async cadastrarLivro (req, res) {
         try{
             const novoLivro = await livro.create(req.body)/
@@ -20,6 +30,15 @@ class livroController {
         }
     }
 
+    static async atualizarLivro (req, res) {
+        try{
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json( {message: "livro atualizado"});
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na atualização`});
+        }
+    };
 };
 
 export default livroController;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,5 +4,8 @@ import livroController from "../controllers/livroController.js";
 const routes = express.Router();
 
 routes.get("/livros", livroController.listarLivros);
+routes.get("/livros/:id", livroController.listarLivroPorId);
+routes.post("/livros", livroController.cadastrarLivro);
+routes.put("/livros/:id", livroController.atualizarLivro);
 
 export default routes;
